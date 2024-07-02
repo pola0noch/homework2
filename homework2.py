537c4fc0e359cf95519f826d89e13e6144966aa9
pola0noch <klayriddell@gmail.com>
2024-07-02 20:01:10 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+#урок "Переменные"
+
+homework_count = 12
+hour_count = 1.5
+course_name = "Python"
+time_for_task = hour_count / homework_count
+print("Курс: " + course_name + ", всего задач " + str(homework_count) + ", затрачено часов: " + str(hour_count) + ", среднее время выполнения " + str(time_for_task))
+
+
